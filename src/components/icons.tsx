3ca3d57719fcea66599c3bbd0377a71b043fb20a
Paul Chavez <paulchavezromero20@gmail.com>
2024-08-03 15:37:31 -0500

    fix: syntax errors solved

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,23 +9,23 @@ export const LinkedinIcon = () => (
     <path
       d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8Z"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M6 9H2V21H6V9Z"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -41,16 +41,16 @@ export const GithubIcon = () => (
     <path
       d="M14.9998 22V18C15.1389 16.7473 14.7797 15.4901 13.9998 14.5C16.9998 14.5 19.9998 12.5 19.9998 9C20.0798 7.75 19.7298 6.52 18.9998 5.5C19.2798 4.35 19.2798 3.15 18.9998 2C18.9998 2 17.9998 2 15.9998 3.5C13.3598 3 10.6398 3 7.99979 3.5C5.99979 2 4.99979 2 4.99979 2C4.69979 3.15 4.69979 4.35 4.99979 5.5C4.27167 6.51588 3.91827 7.75279 3.99979 9C3.99979 12.5 6.99979 14.5 9.99979 14.5C9.60979 14.99 9.31979 15.55 9.14979 16.15C8.97979 16.75 8.92979 17.38 8.99979 18V22"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M9 18C4.49 20 4 16 2 16"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -66,9 +66,9 @@ export const ChevronBack = () => (
     <path
       d="M5 9L1 5L5 1"
       stroke="black"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -82,8 +82,8 @@ export const BehanceIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M11.5 8.5C11.5 9.39629 11.1631 10.2139 10.609 10.8331C11.7536 11.6489 12.5 12.9873 12.5 14.5C12.5 16.9853 10.4853 19 8 19H3.5C2.67157 19 2 18.3284 2 17.5V5.5C2 4.67157 2.67157 4 3.5 4H7.5C9.70914 4 11.5 5.79086 11.5 8V8.5ZM4 10V6H7.5C8.60457 6 9.5 6.89543 9.5 8V8.5C9.5 9.32843 8.82843 10 8 10H4ZM4 12H8C9.38071 12 10.5 13.1193 10.5 14.5C10.5 15.8807 9.38071 17 8 17H4V12Z"
       fill="#000000"
     />
@@ -92,8 +92,8 @@ export const BehanceIcon = () => (
       fill="#000000"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M17.75 8C14.9085 8 12.5 10.1823 12.5 13V14C12.5 16.8177 14.9085 19 17.75 19C19.2651 19 20.3622 18.5636 21.0967 18.0909C21.5388 17.8064 21.8859 17.4599 22.2201 17.0618C22.5304 16.6049 22.4115 15.983 21.9546 15.6727C21.5022 15.3655 20.8879 15.4791 20.5747 15.925C20.4225 16.1191 20.2201 16.2767 20.0143 16.4091C19.5835 16.6864 18.8592 17 17.75 17C15.8971 17 14.5 15.6006 14.5 14H22C22.5523 14 23 13.5523 23 13C23 10.1823 20.5915 8 17.75 8ZM20.8128 12C20.3731 10.8586 19.2014 10 17.75 10C16.2986 10 15.1269 10.8586 14.6872 12H20.8128Z"
       fill="#000000"
     />
@@ -112,16 +112,16 @@ export const LoadingIcon = ({ className }: any) => (
       <path
         d="M21 2V8H15"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20.9998 12.9999C20.7875 15.0058 19.9074 16.8823 18.5009 18.3281C17.0943 19.774 15.2428 20.7054 13.2435 20.973C11.2442 21.2405 9.21297 20.8287 7.47576 19.8035C5.73855 18.7783 4.39609 17.1993 3.66381 15.3198C2.93153 13.4402 2.85187 11.3692 3.43763 9.43898C4.02339 7.50876 5.2406 5.83127 6.89394 4.67571C8.54728 3.52014 10.5409 2.95348 12.5548 3.06668C14.5688 3.17987 16.4864 3.96635 17.9998 5.2999L20.9998 7.9999"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   </div>
