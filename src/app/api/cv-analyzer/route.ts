936bce8605c8dbb3220259d074292dd8517e3546
Paul Chavez <paulchavezromero20@gmail.com>
2024-08-04 19:21:53 -0500

    chore(analyzer): max duration updated to 60 seg

--- src/app/api/cv-analyzer/route.ts
+++ src/app/api/cv-analyzer/route.ts
@@ -2,6 +2,7 @@ import getContext from "@/server/context";
 import cvAnalyzerController from "@/server/controllers/cv_analyzer";
 
 export const dynamic = "force-dynamic";
+export const maxDuration = 60;
 
 export async function POST(req: Request) {
   try {
